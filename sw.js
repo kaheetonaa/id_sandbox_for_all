5cc66b1865effcbdf61fc9dd1c7e2784c8e4e468
Quang Huy <kaheetonaa@gmail.com>
2024-10-18 12:07:14 +0200

    pwa

---
+++ sw.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+      caches.open('iDSandbox4All').then((cache) => cache.addAll([
+        'index.html',
+        'icon/icon.png',
+        'dist/iD.js',
+        'dist/iD.css',
+      ])),
+    );
+  });
+  
+  self.addEventListener('fetch', (e) => {
+    console.log(e.request.url);
+    e.respondWith(
+      caches.match(e.request).then((response) => response || fetch(e.request)),
+    );
+  });
