af28630d9a9fd0edba1ee56985f63932854ad0e7
kaheetonaa <61049296+kaheetonaa@users.noreply.github.com>
2025-01-20 02:11:03 +0100

    update land

--- dist/iD_beta.js
+++ dist/iD_beta.js
@@ -77550,7 +77550,7 @@
     apiUrl: apiUrlroot,
     client_id: osmApiConnections[0].client_id,
     scope: "read_prefs write_prefs write_api read_gpx write_notes",
-    redirect_uri: redirectPath.replace('index_beta.html','') + "land_beta.html",
+    redirect_uri: redirectPath.replace('index_beta.html','') + "land.html",
     loading: authLoading,
     done: authDone
   });
